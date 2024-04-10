e5e2c34c909f723c7c8a56592a71cf2cdbc25c03
sujan khatri <sujansince2059@gmail.com>
2024-04-10 07:34:24 +0545

    adding genre POST

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const genres = [
   { id: 6, name: "Sci-Fi" },
   { id: 7, name: "Thriller" },
 ];
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("this is genre homepage");
 });
@@ -32,4 +32,27 @@ app.get("/genres/:gid", (req, res) => {
   genre
     ? res.send(genre)
     : res.status(404).send("Genre is not found with this ID");
+});
+
+// adding genre
+
+// defining schemavalidator
+function schemaValid(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+}
+
+app.post("/genres", (req, res) => {
+  const { error } = schemaValid(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+  console.log(genres);
 });
